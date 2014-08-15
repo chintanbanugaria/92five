f8a5d25170e2c3d391698b3c1806c8a89cfbfdce
David Pearson <p8.david@gmail.com>
2014-08-15 11:06:34 -0400

    Removed duplicate JS/CSS assets

--- app/views/install/adminaccount.blade.php
+++ app/views/install/adminaccount.blade.php
@@ -4,16 +4,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>92five App - Admin Account</title>
-<!-- Bootstrap -->
-<link href="css/bootstrap.css" rel="stylesheet">
-<link href="css/bootstrap-responsive.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
-<link href="css/custom.css" rel="stylesheet">
-<script src="js/jquery-1.7.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.complexify.js"></script>
-<script src="js/jquery.complexify.banlist.js"></script>
-<script src="js/registration.js"></script>
 <!-- CSS -->
 {{ HTML::style('assets/css/bootstrap/bootstrap.css') }}
 {{ HTML::style('assets/css/bootstrap/bootstrap-responsive.css') }}
